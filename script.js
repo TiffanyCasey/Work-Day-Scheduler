4a16e54781373661d1c48a7ba295b28078f09373
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-03-30 17:28:06 -0400

    added data attributes for each html ID and created a function that uses moment to identify what hour the rows represent in order to change the color of the row

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
-var todoInput = document.querySelectorAll(".todo-text");
-var btn = document.querySelectorAll(".btn");
+// Variables
+var saveBtn = $(".saveBtn");
+var currentHour = moment().format("HH"); // Variable for current hour 
+var currentHourInt = parseInt(currentHour); // Parse it so that hour returns as an integer
 
-//Load jQuery
-$(document).ready(function() {
+// Set data attributes to each hour input element so that the function below can assign a color to each row based on the current hour
+$("#9Row").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
+$("#10Row").attr("data-time", moment("10:00 am", "hh:mm a").format("HH"));
+$("#11Row").attr("data-time", moment("11:00 am", "hh:mm a").format("HH"));
+$("#12Row").attr("data-time", moment("12:00 pm", "hh:mm a").format("HH"));
+$("#1Row").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
+$("#2Row").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
+$("#3Row").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
+$("#4Row").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
+$("#5Row").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
+
+//start jQuery 
+$(document).ready(function () {
 
   // Show Date and Time in Header 
-  $('#currentDay').append();
+  $("#currentDay").append();
 
-  function addDate() {
-  $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm a'));
+  function addDate() { 
+  $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm a'));
+  
   } setInterval(addDate, 1000);
 
+  // Change color in each row by adding in am attribute (above) for each input line to reflect the current hour 
+  for (var i = 0; i <= 12; i++) {  
+
+      var inputHour = $("#" + i + "Row").attr("data-time"); // Variable for the hour of the row 
+      var inputHourInt = parseInt(inputHour); // Parse it so that hour returns as an integer
+
+      if (currentHourInt === inputHourInt) {
+          $("#" + i + "Row").addClass("present"); // Applies red color if within the present hour 
+      }
+      if (currentHourInt > inputHourInt) { // Applies grey color if hour is in the future 
+          $("#" + i + "Row").addClass("past");
+      }
+      if (currentHourInt < inputHourInt) { // Applies green color if hour is in the future 
+          $("#" + i + "Row").addClass("future");
+      }
+    }
 });
-
-
-  // Change the color scheme for the given hours of the day
-  
-  var repeater;
-
-  function showcurrenttime()
-  {
-      //$('.crnttym').text(moment().format('MMMM Do YYYY, h:mm a'));
-      $('.todoInput').each(function (i, el)
-      {
-          if ($(this).data('time') < moment().format('YYYYMMDDHHmm'))
-          {
-              $(this).parent().addClass('.past');
-          };
-          if ($(this).data('time') > moment().format('YYYYMMDDHHmm') && $(this).data('time') < moment().add('h',1).format('YYYYMMDDHHmm'))
-          {
-              $(this).parent().addClass('.present');
-          };
-      });
-      repeater = setTimeout(showcurrenttime);
-  }
-  showcurrenttime();
-
-
-
-
-
-
-
-
